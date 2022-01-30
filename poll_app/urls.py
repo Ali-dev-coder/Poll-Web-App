d3f10820ac959f45a3ffdcdb4dbc28b64ff7dd6b
Ali Raza Shah(Software Engineer) <57613661+Ali-dev-coder@users.noreply.github.com>
2022-01-30 22:13:57 +0500

    Update urls.py

--- poll_app/urls.py
+++ poll_app/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     
-    path('home/',views.home, name = "home"),
+    path('',views.home, name = "home"),
     path('create/',views.create, name = "create"),
     path('vote/<poll_id>/',views.vote, name = "vote"),
     path('result/<poll_id>/',views.result, name = "result")
